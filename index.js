f587143ca99b4c85b95051c79d63259d89d26895
Protap Ghosh <protapg518@gmail.com>
2023-06-09 12:45:09 +0600

    added navbar

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ async function run() {
    try {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
+
+      
+
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log(
@@ -32,12 +35,10 @@ async function run() {
       );
    } finally {
       // Ensures that the client will close when you finish/error
-      await client.close();
+      // await client.close();
    }
 }
 run().catch(console.dir);
-
-
 
 app.get('/', (req, res) => {
    res.send('Skill Up Camp is running...')
